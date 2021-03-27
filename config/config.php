cef3966f716e201653b4ff4dda8de801f63778f3
Marcin Wrzos <marwrz36@gmail.com>
2021-03-27 19:09:23 +0100

    Config file & composer

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'user_notes');
+define('DB_PASS', '12345');
+define('DB_NAME', 'ai_lab10');
+define('STARTING_URL', '/iQchnia');
+define('STATIC_URL', 'http://localhost:8888/iQchnia-AI/public/');
+define('STATIC_COMPONENT', '/Desktop/_iQchnia/iQchnia/views');
--- public/index.php
+++ public/index.php
@@ -1,3 +1,8 @@
 <?php
 
-session_start()
+session_start();
+require_once realpath("../vendor/autoload.php");
+require_once("../config/config.php");
+require_once("../utility/debugFunction.php");
+
+dump('data');
