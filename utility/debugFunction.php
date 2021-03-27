c5ab53506d5145f89cc6419d797ee7db029cb216
Marcin Wrzos <marwrz36@gmail.com>
2021-03-27 18:48:49 +0100

    Debug Fn

---
+++ utility/debugFunction.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+
+ini_set('display_errors', '1');
+
+function dump($data)
+{
+    echo '<br/><div 
+    style="
+      display: inline-block;
+      padding: 2px 10px;
+      border: 1px dashed gray;
+      background: lightgray;
+      margin:5px;
+    "
+  >
+  <pre>';
+    print_r($data);
+    echo '</pre>
+  </div>
+  <br/>';
+}
