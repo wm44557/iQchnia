09f2091f3396aadeeb6fa8574588d9374700680e
Marcin Wrzos <marwrz36@gmail.com>
2021-05-05 01:44:20 +0200

    AddRecipe form and Recipe Model

--- controllers/userController.php
+++ controllers/userController.php
@@ -6,6 +6,8 @@ namespace app\controllers;
 
 use app\Router;
 use app\models\User;
+use app\models\Recipe;
+
 use app\utility\Redirect;
 use app\utility\Permissions;
 
@@ -34,7 +36,32 @@ class userController
     {
         Permissions::check("user");
         $user = new User();
-        $router->render("pages/user/dodajprzepis", []);
+        $recipe = new Recipe();
+
+        if (isset($_POST['zapisz_dane'])) {
+            if (
+                empty($_POST['description'])
+                && empty($_POST['title'])
+                && empty($_POST['difficulty'])
+                && empty($_POST['calories'])
+                && empty($_POST['diets'])
+                && empty($_POST['creator'])
+            ) {
+            }
+            $newData = $_POST;
+
+            $recipe->createRecipe($newData);
+        }
+        dump($_POST);
+
+
+        $router->render("pages/user/dodajprzepis", [
+            'dane' => $recipe->getAllIngredients(),
+            'category' => $recipe->getAllCategories(),
+            'difficulties' => $recipe->getAllDifficulties(),
+            'diets' => $recipe->getAllDiets()
+
+        ]);
     }
     public function edytujdane($router)
     {
@@ -48,30 +75,25 @@ class userController
 
 
         if (isset($_POST['zapisz_dane'])) {
-            if(!empty($_POST['new_login']) && !empty($_POST['new_email'])) {
+            if (!empty($_POST['new_login']) && !empty($_POST['new_email'])) {
 
-                if(empty($_POST['old_password']) && empty($_POST['new_password']) && empty($_POST['new_password2'])) {
+                if (empty($_POST['old_password']) && empty($_POST['new_password']) && empty($_POST['new_password2'])) {
                     $newData['new_password'] = $result->password;
                     $user->editUserData($newData);
                     $_SESSION['user_login'] = $newData['new_login'];
                     $_SESSION['user_email'] = $newData['new_email'];
-                }
-
-                else if(!empty($_POST['old_password']) && $_POST['old_password'] == $result->password) {
-                    if($_POST['new_password'] == $_POST['new_password2']) {
+                } else if (!empty($_POST['old_password']) && $_POST['old_password'] == $result->password) {
+                    if ($_POST['new_password'] == $_POST['new_password2']) {
                         $user->editUserData($newData);
                         $_SESSION['user_login'] = $newData['new_login'];
                         $_SESSION['user_email'] = $newData['new_email'];
-                    }
-                    else {
+                    } else {
                         $errors['edited'] = "Nowo ustawione hasła muszą się pokrywać";
                     }
-                }
-                else {
+                } else {
                     $errors['edited'] = "Wpisane przez Ciebie hasło jest niepoprawne";
                 }
-            }
-            else {
+            } else {
                 $errors['edited'] = "Pola login i email nie mogą pozostać puste";
             }
             if (!isset($errors['edited']))
@@ -79,9 +101,8 @@ class userController
         }
 
         $router->render("pages/user/edytujdane", [
-            'dane'=>$result,
+            'dane' => $result,
             'errors' => $errors,
         ]);
-
     }
 }
---
+++ models/Recipe.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\models;
+
+use app\utility\Database;
+
+class Recipe
+{
+
+    public function getAllIngredients()
+    {
+        $this->conn = new Database();
+        $this->conn->query('SELECT * FROM ingredients');
+        $result = $this->conn->resultAll();
+        return $result;
+    }
+    public function getAllCategories()
+    {
+        $this->conn = new Database();
+        $this->conn->query('SELECT * FROM categories');
+        $result = $this->conn->resultSet();
+        return $result;
+    }
+    public function getAllDifficulties()
+    {
+        $this->conn = new Database();
+        $this->conn->query('SELECT * FROM difficulties');
+        $result = $this->conn->resultSet();
+        return $result;
+    }
+    public function getAllDiets()
+    {
+        $this->conn = new Database();
+        $this->conn->query('SELECT * FROM diets');
+        $result = $this->conn->resultSet();
+        return $result;
+    }
+    public function createRecipe($dataRegister)
+    {
+        $this->conn = new Database();
+        $this->conn->query("INSERT INTO `recipes`(`title`, `description`, `creator`,`difficulty`, `calories`, `diet`, `category`,`photo`) VALUES (:title,:description,:creator,:difficulty,:calories,:diet,:category,'123')");
+        $this->conn->bindValue("title", $dataRegister['title']);
+        $this->conn->bindValue("description", $dataRegister['description']);
+        $this->conn->bindValue("creator", $dataRegister['creator']);
+        $this->conn->bindValue("difficulty", $dataRegister['difficulty']);
+        $this->conn->bindValue("calories", $dataRegister['calories']);
+        $this->conn->bindValue("diet", $dataRegister['diet']);
+        $this->conn->bindValue("category", $dataRegister['category']);
+        $this->conn->execute();
+    }
+}
